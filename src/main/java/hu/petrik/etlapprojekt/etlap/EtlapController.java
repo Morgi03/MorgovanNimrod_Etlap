569cc9cfa56071d21bc84aacdc1a86725c4fe346
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-15 09:19:30 +0100

    Etel class elkészítése és Controller frissítése

--- src/main/java/hu/petrik/etlapprojekt/etlap/EtlapController.java
+++ src/main/java/hu/petrik/etlapprojekt/etlap/EtlapController.java
@@ -1,14 +1,47 @@
 package hu.petrik.etlapprojekt.etlap;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class EtlapController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableView<Etel> etlapTable;
+    @FXML
+    private Spinner<Integer> forintSpinner;
+    @FXML
+    private Button torlesButton;
+    @FXML
+    private TableColumn<Etel, String> nameCol;
+    @FXML
+    private ListView<String> listview;
+    @FXML
+    private Button ujEtelFelveteleButton;
+    @FXML
+    private Spinner<Integer> szazalekSpinner;
+    @FXML
+    private TableColumn<Etel, String> categoryCol;
+    @FXML
+    private Button szazalekEmelesButton;
+    @FXML
+    private Button forintEmelesButton;
+    @FXML
+    private TableColumn<Etel, Integer> priceCol;
+
+    @FXML
+    public void ujEtelFelveteleClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void torlesClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void forintEmelesClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void szazalekEmelesClick(ActionEvent actionEvent) {
     }
 }
